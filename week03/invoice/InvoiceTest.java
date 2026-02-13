3a2384e33adcd031cd92922a54befd592d99ac6c
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-02-13 13:31:25 +0400

    Part E

---
+++ week03/invoice/InvoiceTest.java
@@ -0,0 +1,19 @@
+package week03.invoice;
+import week03.invoice.Invoice;
+
+public class InvoiceTest {
+    public static void main(String[] args) {
+        // Make a valid object (Constructor is working)
+        Invoice valid = new Invoice("A48", "Bearings", 4, 0.25);
+        
+        System.out.printf("\nParts %s are %s bought %d times with a price of $%.2f per item. This costs $%.2f in total.\n",
+         valid.getPartNumber(), valid.getPartDescription(), valid.getItemQuantity(), valid.getPricePerItem(), valid.getInvoiceAmount());
+        
+         // Test for non-zero inputs
+        Invoice invalid = new Invoice("B12", "Large bearings", -12, -0.01);
+              
+        System.out.printf("\nParts %s are %s bought %d times with a price of $%.2f per item. This costs $%.2f in total.\n\n",
+         invalid.getPartNumber(), invalid.getPartDescription(), invalid.getItemQuantity(), invalid.getPricePerItem(), invalid.getInvoiceAmount());
+
+    }
+}
