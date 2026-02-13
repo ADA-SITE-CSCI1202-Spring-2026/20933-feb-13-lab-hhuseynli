52d7b0dd9e5dcf3a8167331781056828936ba32e
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-02-13 13:33:53 +0400

    Test all setters

--- week03/invoice/InvoiceTest.java
+++ week03/invoice/InvoiceTest.java
@@ -12,12 +12,14 @@ public class InvoiceTest {
          // Test for non-zero inputs
         Invoice invalid = new Invoice("B12", "Large bearings", -12, -0.01);
               
-        System.out.printf("\nParts %s are %s bought %d times with a price of $%.2f per item. This costs $%.2f in total.\n\n",
+        System.out.printf("\nParts %s are %s bought %d times with a price of $%.2f per item. This costs $%.2f in total.\n",
          invalid.getPartNumber(), invalid.getPartDescription(), invalid.getItemQuantity(), invalid.getPricePerItem(), invalid.getInvoiceAmount());
         
         invalid.setItemQuantity(10);
         invalid.setPricePerItem(5.423);
-        
+        invalid.setPartNumber("VP00");
+        invalid.setPartDescription("Valid Parts");
+
         System.out.printf("\nParts %s are %s bought %d times with a price of $%.2f per item. This costs $%.2f in total.\n\n",
          invalid.getPartNumber(), invalid.getPartDescription(), invalid.getItemQuantity(), invalid.getPricePerItem(), invalid.getInvoiceAmount());
 
